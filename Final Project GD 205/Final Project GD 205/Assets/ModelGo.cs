fada21c0e18f0e0d025953ba92755ddd208faf20
Kareem Clarke <clarke.kareem3@gmail.com>
2016-11-15 17:09:39 -0500

    Final Game (Prototype)
    Final Game Project

---
+++ Final Project GD 205/Final Project GD 205/Assets/ModelGo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelGo : MonoBehaviour {
+	Animator myAnimator;
+	int Animstate;
+
+
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) {
+			Animstate = 1;
+
+			myAnimator.SetInteger ("currentState", Animstate);
+		}
+	}
+}
